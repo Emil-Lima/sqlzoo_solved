27d1099835a89ecfd2d028dced354c7727524a5d
J-Rozas <jrozaslima@gmail.com>
2021-01-26 22:14:03 +0000

    Added ninth exercise.

---
+++ select_from_nobel/ninth_exercise.sql
@@ -0,0 +1,6 @@
+/*
+9. Show the year, subject, and name of winners for 1980 excluding Chemistry and Medicine
+*/
+
+SELECT yr, subject, winner FROM nobel
+WHERE yr = 1980 AND subject NOT IN ('Chemistry', 'Medicine');
