c44e25b808fc1ad2de328c330de337281ed9062b
J-Rozas <jrozaslima@gmail.com>
2021-01-26 18:53:48 +0000

    Added fouth exercise.

---
+++ join/fouth_exercise.sql
@@ -0,0 +1,10 @@
+/*
+4. Use the same JOIN as in the previous question.
+
+Show the team1, team2 and player for every goal scored by a player called Mario player LIKE 'Mario%'
+*/
+
+SELECT team1, team2, player FROM game
+JOIN goal
+ON goal.matchid = game.id
+WHERE player LIKE 'Mario%';
