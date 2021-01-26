1ce1ee19f8a5d993eaa00a20dfe7e796bf11074f
J-Rozas <jrozaslima@gmail.com>
2021-01-26 22:07:56 +0000

    Added fifth exercise.

---
+++ select_from_nobel/fifth_exercise.sql
@@ -0,0 +1,7 @@
+/*
+5. Show all details (yr, subject, winner) of the Literature prize winners for 1980 to 1989 inclusive.
+*/
+
+SELECT * FROM nobel
+WHERE subject = 'Literature' AND
+yr BETWEEN 1980 AND 1989;
