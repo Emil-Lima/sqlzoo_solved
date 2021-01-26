7700e82309df7da062476dfdb348fd9121c634f9
J-Rozas <jrozaslima@gmail.com>
2021-01-26 18:56:01 +0000

    Added fifth exercise

---
+++ join/fifth_exercise.sql
@@ -0,0 +1,11 @@
+/*
+5. The table eteam gives details of every national team including the coach. You can JOIN goal to
+eteam using the phrase goal JOIN eteam on teamid=id
+
+Show player, teamid, coach, gtime for all goals scored in the first 10 minutes gtime<=10
+*/
+
+SELECT player, teamid, coach, gtime FROM goal
+JOIN eteam
+ON eteam.id = goal.teamid
+WHERE goal.gtime <= 10;
