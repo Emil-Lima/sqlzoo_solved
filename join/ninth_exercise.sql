61d067b6fb7c19c5345a03f84f7eedbc2b9885da
J-Rozas <jrozaslima@gmail.com>
2021-01-26 19:17:01 +0000

    Styled the code.

--- join/ninth_exercise.sql
+++ join/ninth_exercise.sql
@@ -2,7 +2,7 @@
 9. Show teamname and the total number of goals scored.
 */
 
-SELECT teamname, COUNT(player) as num_goals FROM eteam
+SELECT teamname, COUNT(player) AS num_goals FROM eteam
 JOIN goal
 ON goal.teamid = eteam.id
 GROUP BY teamname;
