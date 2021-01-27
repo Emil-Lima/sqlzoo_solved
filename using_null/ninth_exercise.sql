c36ea5cc975bd1934e619e2caf16e69ee56b47ae
J-Rozas <jrozaslima@gmail.com>
2021-01-27 14:26:50 +0000

    Added ninth exercise.

---
+++ using_null/ninth_exercise.sql
@@ -0,0 +1,5 @@
+/*
+9. Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2 and 'Art' otherwise.
+*/
+
+SELECT name, CASE WHEN teacher.dept = 1 OR teacher.dept = 2 THEN 'Sci' ELSE 'Art' END FROM teacher;
