3ab0b775a4e3a71427f366cb0ed4dbb0e5dd170e
J-Rozas <jrozaslima@gmail.com>
2021-01-27 14:28:48 +0000

    Improved readability of output table.

--- using_null/ninth_exercise.sql
+++ using_null/ninth_exercise.sql
@@ -2,4 +2,4 @@
 9. Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2 and 'Art' otherwise.
 */
 
-SELECT name, CASE WHEN teacher.dept = 1 OR teacher.dept = 2 THEN 'Sci' ELSE 'Art' END FROM teacher;
+SELECT name, CASE WHEN teacher.dept = 1 OR teacher.dept = 2 THEN 'Sci' ELSE 'Art' END AS type_dept FROM teacher;
