a55de33805152f5435328b3e8153245bbbc7c9ea
J-Rozas <jrozaslima@gmail.com>
2021-01-27 14:11:58 +0000

    Added fifth exercise.

---
+++ using_null/fifth_exercise.sql
@@ -0,0 +1,7 @@
+/*
+5. Use COALESCE to print the mobile number.
+ Use the number '07986 444 2266' if there is no number given. 
+ Show teacher name and mobile number or '07986 444 2266'
+*/
+
+SELECT name, COALESCE(mobile, '07986 444 2266') AS number FROM teacher; 
